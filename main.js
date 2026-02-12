0a4a350058adb3f3dc3541a9a8ff0663230da87f
dagoat <andres.valenzuela133003@gmail.com>
2026-02-12 14:28:35 -0500

    COMMIT4

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 let intentos = 0;
 
-console.log("guess the number from 1 to 10");
+console.log("Adivina el número del 1 al 10");
 
+while (true) {
+
+    let numeroUsuario = prompt("Enter a number:");
+
+    if (numeroUsuario === null) {
+        console.log("game cancelled");
+        break;
+    }
+
+
+}
